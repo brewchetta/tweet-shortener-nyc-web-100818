b46d606c86d97f77bdc7d56ca0d649de913f63f7
Chett Tiller <chett.tiller@gmail.com>
2018-09-24 17:07:23 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,11 +1,9 @@
-# Write your code here.
-
 dictionary = {
   'hello' => 'hi'
-  'hello' => 'hi'
-  'hello' => 'hi'
-  'hello' => 'hi'
-  'hello' => 'hi'
-  'hello' => 'hi'
-  'hello' => 'hi'
+  'to, two, too' => '2'
+  'for' => '4'
+  'be' => 'b'
+  'you' => 'u'
+  'at' => '@'
+  'and' => '&'
 }
