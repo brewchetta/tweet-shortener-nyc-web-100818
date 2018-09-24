38e226ccb827bd15cc17fb1024e9f27199b81c23
Chett Tiller <chett.tiller@gmail.com>
2018-09-24 17:48:28 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -16,7 +16,7 @@ def word_substituter(tweet)
   short_tweet = []
   tweet.split(' ').each do |word|
     dictionary.each do |k,v|
-      word.replace v
+      word.replace v if k == word
     end
   end
   short_tweet.join(" ")
